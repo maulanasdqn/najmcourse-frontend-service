c7a1b9a4f29b5f0068abb1e4c3caea729feee4d8
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2025-05-08 19:55:59 +0700

    feat: mapping permissions

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,6 +12,70 @@ const mappingRoutePermissions = [
     path: ROUTES.iam.users.list,
     permissions: [PERMISSIONS.USERS.READ_LIST_USERS],
   },
+  {
+    path: ROUTES.iam.users.create,
+    permissions: [PERMISSIONS.USERS.CREATE_USERS],
+  },
+  {
+    path: ROUTES.iam.users.update,
+    permissions: [PERMISSIONS.USERS.UPDATE_USERS],
+  },
+  {
+    path: ROUTES.iam.users.detail,
+    permissions: [PERMISSIONS.USERS.READ_DETAIL_USERS],
+  },
+  {
+    path: ROUTES.iam.roles.list,
+    permissions: [PERMISSIONS.ROLES.READ_LIST_ROLES],
+  },
+  {
+    path: ROUTES.iam.roles.create,
+    permissions: [PERMISSIONS.ROLES.CREATE_ROLES],
+  },
+  {
+    path: ROUTES.iam.roles.update,
+    permissions: [PERMISSIONS.ROLES.UPDATE_ROLES],
+  },
+  {
+    path: ROUTES.iam.roles.detail,
+    permissions: [PERMISSIONS.ROLES.READ_DETAIL_ROLES],
+  },
+  {
+    path: ROUTES.iam.permissions.list,
+    permissions: [PERMISSIONS.PERMISSIONS.READ_LIST_PERMISSIONS],
+  },
+  {
+    path: ROUTES.exams.sessions.list,
+    permissions: [PERMISSIONS.SESSIONS.READ_LIST_SESSIONS],
+  },
+  {
+    path: ROUTES.exams.sessions.create,
+    permissions: [PERMISSIONS.SESSIONS.CREATE_SESSIONS],
+  },
+  {
+    path: ROUTES.exams.sessions.update,
+    permissions: [PERMISSIONS.SESSIONS.UPDATE_SESSIONS],
+  },
+  {
+    path: ROUTES.exams.sessions.detail,
+    permissions: [PERMISSIONS.SESSIONS.READ_DETAIL_SESSIONS],
+  },
+  {
+    path: ROUTES.exams.tests.list,
+    permissions: [PERMISSIONS.TESTS.READ_LIST_TESTS],
+  },
+  {
+    path: ROUTES.exams.tests.create,
+    permissions: [PERMISSIONS.TESTS.CREATE_TESTS],
+  },
+  {
+    path: ROUTES.exams.tests.update,
+    permissions: [PERMISSIONS.TESTS.UPDATE_TESTS],
+  },
+  {
+    path: ROUTES.exams.tests.detail,
+    permissions: [PERMISSIONS.TESTS.READ_DETAIL_TESTS],
+  },
 ];
 
 const mappingPublicRoutes = ["/auth/login", "/auth/forgot"];
@@ -19,7 +83,6 @@ const mappingPublicRoutes = ["/auth/login", "/auth/forgot"];
 export const middleware = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const pathname = url.pathname;
-
   const session = SessionUser.get();
   const session_token = SessionToken.get();
   const token = session_token?.token?.access_token;
